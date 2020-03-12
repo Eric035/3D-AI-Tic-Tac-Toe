224aef57b0e2c1089d42ce27b4a91382d779ee61
Eric Leung <30438439+Eric035@users.noreply.github.com>
2020-03-12 09:38:54 -0400

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,11 +1,6 @@
 // Header file for functions
-
 void greetAndInstruct();
 void displayBoard(char board[]);
-// void isFull(char board[]);
 bool checkIfLegal (int cellNbre, char board[]);
-//bool checkHorizontal (char board[], char whoWon[]);
-//bool checkVertical (char board[], char whoWon[]);
-//bool checkDiagonal (char board[], char whoWon[]);
 bool checkWinner(char board[]);
 void computerMove(char board[]);
